533cd0e1775ab01e75c19fd88b553a0ced0e68b7
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-06-28 09:34:19 +0800

    add middleware.js and test code

---
+++ __test__/WebClient3.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebClient3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GridView1.DataSource = getAllTopics();
+        GridView1.DataBind();
+    }
+
+    protected List<Topic> getAllTopics()
+    {
+        string SERVICE_URL = "http://10.1.110.111:3000/api/topics";
+        string retJSON = "";
+
+        try
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers.Add("Content-Type", "application/json; charset=utf-8");
+
+                retJSON = webClient.DownloadString(SERVICE_URL);
+                ReturnType res = JsonConvert.DeserializeObject<ReturnType>(retJSON);
+
+                Label1.Text = String.Format("OK ({0})", res.topics.Count);
+                return res.topics;
+            }
+        }
+        catch (WebException ex)
+        {
+            throw ex;
+        }
+    }
+
+    public class Topic
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+
+    public class ReturnType
+    {
+        public List<Topic> topics { get; set; }
+    }
+}
--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -9,13 +9,19 @@ export default function RemoveBtn({ id }) {
     const confirmed = confirm("Are you sure?");
 
     if (confirmed) {
-      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
-        method: "DELETE",
-      });
-
-      if (res.ok) {
-        router.refresh();
-      }
+      try {
+        const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+          method: "DELETE",
+        });
+  
+        if (res.ok) {
+          router.refresh();
+        } else {
+          throw new Error("Failed to create a topic");
+        }
+      } catch (error) {
+        console.log(error);
+      }      
     }
   };
 
