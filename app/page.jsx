25045554c6b679f7b474e50273a00a56473b6794
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-06-28 16:24:58 +0800

    pretty much

--- app/page.jsx
+++ app/page.jsx
@@ -3,3 +3,21 @@ import TopicsList from "@/components/TopicsList";
 export default function Home() {
   return <TopicsList />;
 }
+
+/*
+Reference: 
+1. Server Actions: NextJS 13.4's Best New Feature
+   https://youtu.be/czvSZqnpTHs
+2. Step-by-Step Guide: Create a Next.js 13 CRUD App with MongoDB from Scratch
+   https://youtu.be/wNWyMsrpbz0
+3. NEXTjs | Data Fetching
+   https://nextjs.org/docs/app/building-your-application/data-fetching
+4. How to enable cors in Nextjs 13 Route Handlers
+   https://github.com/vercel/next.js/discussions/47933
+5. How to Deploy Nextjs Web Application with PM2
+   https://dykraf.com/blog/deploying-nextjs-web-application-with-pm2
+6. With Docker - Multiple Deployment Environments
+   https://github.com/vercel/next.js/tree/canary/examples/with-docker-multi-env
+7. The Purloined Letter
+   https://poemuseum.org/the-purloined-letter/   
+*/
