10e2d0e398f1b5c8a4b7dd585fe805ad36c98b7c
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-06-30 09:29:56 +0800

    rename

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="max-w-3xl p-4 mx-auto">
+        <div className="max-w-3xl p-4 mx-auto ">
           <Navbar />
           <div className="mt-8">{children}</div>
           <Footer />
