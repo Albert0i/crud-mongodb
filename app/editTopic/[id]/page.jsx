bb8e113ee85c950b09858663378856cee0f9e3fe
Alberto Iong <albert0i@hotmail.com>
2023-07-18 15:30:05 +0800

    trying to add static site generation, but failed...

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,5 +1,5 @@
 import EditTopicForm from "@/components/EditTopicForm";
-import { getTopicById } from "@/actions/topicServerAction"
+import { getTopicById, getTopics } from "@/actions/topicServerAction"
 
 export default async function EditTopic({ params }) {
   const { id } = params;
@@ -8,3 +8,18 @@ export default async function EditTopic({ params }) {
 
   return <EditTopicForm id={id} title={title} description={description} />;
 }
+
+/*
+   Next.js 13: complete guide to Server Components and the App Directory
+   https://makerkit.dev/blog/tutorials/nextjs13
+
+   generateStaticParams
+   https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+*/
+// export async function generateStaticParams() {  
+//   const { topics } = await getTopics();
+
+//   return topics.map(topic => ({
+//     id: topic.id
+//   }))
+// }
